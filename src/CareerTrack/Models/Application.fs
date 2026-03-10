89c90537044caeeb6132275f2078656e08370810
elekvk <elek.viktoria85@gmail.com>
2026-03-10 21:26:35 +0100

    added application model

---
+++ src/CareerTrack/Models/Application.fs
@@ -0,0 +1,10 @@
+﻿namespace CareerTrack.Models
+
+type Application = {
+    Id: int
+    Company: string
+    Position: string
+    DateApplied: System.DateTime
+    Status: string
+    Notes: string
+}
